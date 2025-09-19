bc9335137a27f5d393c038df3fa494dbca6032ef
Shardul Dhekane <144622925+ProbablyItsSpirit@users.noreply.github.com>
2025-09-19 10:00:38 +0530

    Redesign

--- src/components/PropertiesPage.js
+++ src/components/PropertiesPage.js
@@ -37,20 +37,27 @@ const PropertiesPage = ({ homes, togglePop }) => {
         <hr />
 
         {filteredHomes.length > 0 ? (
-          <div className='cards'>
+          <div className='properties-list'>
             {filteredHomes.map((home, index) => (
-              <div className='card' key={index} onClick={() => togglePop(home)}>
-                <div className='card__image'>
+              <div className='property-item' key={index} onClick={() => togglePop(home)}>
+                <div className='property-item__image'>
                   <img src={home.image} alt='Home' />
                 </div>
-                <div className='card__info'>
-                  <h4>{home.attributes[0].value} ETH</h4>
-                  <p>
-                    <strong>{home.attributes[2].value}</strong> bds |
-                    <strong>{home.attributes[3].value}</strong> ba |
-                    <strong>{home.attributes[4].value}</strong> sqft
-                  </p>
-                  <p>{home.address}</p>
+                <div className='property-item__info'>
+                  <div className='property-item__header'>
+                    <h4 className='property-item__title'>{home.name}</h4>
+                    <div className='property-item__price'>
+                      <strong>{home.attributes[0].value} ETH</strong>
+                    </div>
+                  </div>
+                  <div className='property-item__details'>
+                    <p>
+                      <strong>{home.attributes[2].value}</strong> bds |
+                      <strong>{home.attributes[3].value}</strong> ba |
+                      <strong>{home.attributes[4].value}</strong> sqft
+                    </p>
+                    <p className='property-item__address'>{home.address}</p>
+                  </div>
                 </div>
               </div>
             ))}
